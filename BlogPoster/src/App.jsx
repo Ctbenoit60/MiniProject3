e4215e3a2a027df97c552580b69724b490a0c1b7
Ctbenoit60 <ctbenoit60@gmail.com>
2024-02-10 16:14:04 -0600

    Linked the signup and login page together

--- BlogPoster/src/App.jsx
+++ BlogPoster/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
+import SignUp from "./pages/SignUpPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route>
     </Routes>
   );
