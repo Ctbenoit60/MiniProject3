f084aa1929c607967b454a21048d7608af17e9e6
Ctbenoit60 <ctbenoit60@gmail.com>
2024-02-10 14:45:39 -0600

    added MUI login template for login page

--- BlogPoster/src/Layout.jsx
+++ BlogPoster/src/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 export default function Layout() {
     return (
-        <div>
+        <div className="p-4">
             <Header />
             <Outlet />
         </div>
