41798fdeceb6c0b5277a0e8765da4928c0cfd6ab
Ctbenoit60 <ctbenoit60@gmail.com>
2024-02-26 07:17:28 -0600

    Removed Header: Not necassary atm

--- BlogPoster/src/App.jsx
+++ BlogPoster/src/App.jsx
@@ -12,6 +12,7 @@ axios.defaults.baseURL = 'http://127.0.0.1:4000';
 
 function App() {
   return (
+    
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
