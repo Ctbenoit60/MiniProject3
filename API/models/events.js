370d832bb67be86954fde4da11abcdba7c682344
Ctbenoit60 <ctbenoit60@gmail.com>
2024-03-06 06:46:15 -0600

    updated PORT in index to 4000

--- API/models/events.js
+++ API/models/events.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const eventSchema = new Schema({
+  id: { type: Number},
   title: { type: String, required: true },
   description: { type: String },
   start: { type: Date, required: true },
-  time: { type: String, required: true },
   // Other fields (location, attendees, reminders, etc.)
 });
 
